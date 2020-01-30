191e55a462e5e8fac1486ff8a4d6d35535420f15
Thore Braun <thore@zerberus.escape.de>
2020-01-30 18:10:57 +0100

    add fields to the cardealer

--- src/de/tuclausthal/isse/softwaretechnik/ws2019/carconfigurator/model/CarDealer.java
+++ src/de/tuclausthal/isse/softwaretechnik/ws2019/carconfigurator/model/CarDealer.java
@@ -1,4 +1,54 @@
 package de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.model;
 
+/*
+Fassade für das model
+ */
+
+import java.util.ArrayList;
+
 public class CarDealer {
+    private ArrayList<Car> cars;
+    private ArrayList<Customer> customers;
+    private ArrayList<Order> orders;
+    private ArrayList<Staffmember> staffmembers;
+
+    public CarDealer(ArrayList<Car> cars, ArrayList<Customer> customers, ArrayList<Order> orders, ArrayList<Staffmember> staffmembers) {
+        this.cars = cars;
+        this.customers = customers;
+        this.orders = orders;
+        this.staffmembers = staffmembers;
+    }
+
+
+    public ArrayList<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
+
+    public ArrayList<Staffmember> getStaffmembers() {
+        return staffmembers;
+    }
+
+    public void setStaffmembers(ArrayList<Staffmember> staffmembers) {
+        this.staffmembers = staffmembers;
+    }
 }
