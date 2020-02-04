793543692290d49c592754c3cadb9da96102b0f0
ya <ya@YaMeN>
2020-02-04 14:13:16 +0100

    Main erstellt

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.controler.impl.ControllerImpl;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		new ControllerImpl();
+	}
+}
