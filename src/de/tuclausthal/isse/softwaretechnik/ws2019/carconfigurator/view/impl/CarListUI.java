18b1d73f2a8b26f219fed685a2bd0fe9470538ca
ya <ya@YaMeN>
2020-02-04 14:17:50 +0100

    CheckBoxes angepasst

--- src/de/tuclausthal/isse/softwaretechnik/ws2019/carconfigurator/view/impl/CarListUI.java
+++ src/de/tuclausthal/isse/softwaretechnik/ws2019/carconfigurator/view/impl/CarListUI.java
@@ -1,9 +1,7 @@
 package de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.view.impl;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
@@ -20,29 +18,28 @@ public class CarListUI extends JFrame {
 	private ImageIcon image3;
 	private ImageIcon image4;
 	private ImageIcon image5;
-	private ImageIcon image6;
+	
 	private JLabel im1Label;
 	private JLabel im2Label;
 	private JLabel im3Label;
 	private JLabel im4Label;
 	private JLabel im5Label;
-	private JLabel im6Label;
 	
 	private JCheckBox im1ChBox;
 	private JCheckBox im2ChBox;
 	private JCheckBox im3ChBox;
 	private JCheckBox im4ChBox;
 	private JCheckBox im5ChBox;
-	private JCheckBox im6ChBox;
+
 	
-	private JButton bestellenButton;
-	private JButton configurierenButton;
+	private JButton orderButton;
+	private JButton configureButton;
 	private JPanel buttonPanel;
 	
 	private JPanel mainPanel;
 	
 	private ActionListener actionListener;
-	
+
 	public CarListUI(ActionListener actionListener) {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.actionListener = actionListener;
@@ -52,12 +49,11 @@ public class CarListUI extends JFrame {
 		this.composeUI();
 	}
 	public void composeUI() {
-		this.titleLabel = new JLabel("Car Liste");
-		this.titlePanel = new JPanel();
-		this.titlePanel.add(this.titleLabel);
-		
+
 		this.mainPanel = new JPanel();
-		this.mainPanel.setLayout(new FlowLayout());
+		this.titleLabel = new JLabel("Autos Liste");
+		this.titleLabel.setBounds(900, 10, 100, 10);
+		this.mainPanel.add(titleLabel);
 		
 		this.image1 = new ImageIcon(getClass().getResource("BMW_X5_3.jpg"));
 		Image img1 = image1.getImage();
@@ -84,24 +80,33 @@ public class CarListUI extends JFrame {
 		Image newImg5 = img5.getScaledInstance(350, 240, java.awt.Image.SCALE_SMOOTH);
 		this.image5 = new ImageIcon(newImg5);
 		
-		this.image6 = new ImageIcon(getClass().getResource("VW_Passat_8.jpg"));
-		Image img6 = image6.getImage();
-		Image newImg6 = img6.getScaledInstance(350, 240, java.awt.Image.SCALE_SMOOTH);
-		this.image6 = new ImageIcon(newImg6);
-		
-		
 		this.im1Label = new JLabel(image1);
+		this.im1Label.setBounds(10, 60, 350, 240);
 		this.im2Label = new JLabel(image2);
+		this.im2Label.setBounds(370, 60, 350, 240);
 		this.im3Label = new JLabel(image3);
+		this.im3Label.setBounds(730, 60, 350, 240);
 		this.im4Label = new JLabel(image4);
+		this.im4Label.setBounds(1090, 60, 350, 240);
 		this.im5Label = new JLabel(image5);
-		this.im6Label = new JLabel(image6);
+		this.im5Label.setBounds(1450, 60, 350, 240);
+		
 		this.im1ChBox = new JCheckBox("BMW X5 3");
+		this.im1ChBox.setBounds(10,310, 100, 20);
+		this.im1ChBox.addActionListener(this.actionListener);
 		this.im2ChBox = new JCheckBox("Audi R8 2");
+		this.im2ChBox.setBounds(370,310, 100, 20);
+		this.im2ChBox.addActionListener(this.actionListener);
 		this.im3ChBox = new JCheckBox("Audi A4 4");
+		this.im3ChBox.setBounds(730 ,310, 100, 20);
+		this.im3ChBox.addActionListener(this.actionListener);
 		this.im4ChBox = new JCheckBox("BMW X6 2");
+		this.im4ChBox.setBounds(1090 ,310, 100, 20);
+		this.im4ChBox.addActionListener(this.actionListener);
 		this.im5ChBox = new JCheckBox("VW Golf 2");
-		this.im6ChBox = new JCheckBox("VW Passat 8");
+		this.im5ChBox.setBounds(1450 ,310, 100, 20);
+		this.im5ChBox.addActionListener(this.actionListener);
+		
 		this.mainPanel.add(im1Label);
 		this.mainPanel.add(im1ChBox);
 		this.mainPanel.add(im2Label);
@@ -112,20 +117,135 @@ public class CarListUI extends JFrame {
 		this.mainPanel.add(im4ChBox);
 		this.mainPanel.add(im5Label);
 		this.mainPanel.add(im5ChBox);
-		this.mainPanel.add(im6Label);
-		this.mainPanel.add(im6ChBox);
+		this.mainPanel.setSize(1000,1000);
+		
+		this.orderButton = new JButton("bestellen");
+		this.configureButton = new JButton("configurieren");
+		this.orderButton.setBounds(900, 500, 100, 50);
+		this.orderButton.addActionListener(this.actionListener);
+		this.configureButton.setBounds(1010, 500, 120, 50);
+		this.configureButton.addActionListener(this.actionListener);
+		this.mainPanel.add(this.orderButton);
+		this.mainPanel.add(this.configureButton);
+		
 		
 		this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
-		this.getContentPane().add(this.titlePanel);
 		this.getContentPane().add(this.mainPanel);
 		this.pack();
+		
+		this.im1ChBox.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(im1ChBox.isSelected()) {
+					im2ChBox.setEnabled(false);
+					im3ChBox.setEnabled(false);
+					im4ChBox.setEnabled(false);
+					im5ChBox.setEnabled(false);
+				}else if(!im1ChBox.isSelected()) {
+					im2ChBox.setEnabled(true);
+					im3ChBox.setEnabled(true);
+					im4ChBox.setEnabled(true);
+					im5ChBox.setEnabled(true);
+				}
+				
+			}
+		});
+		this.im2ChBox.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(im2ChBox.isSelected()) {
+					im1ChBox.setEnabled(false);
+					im3ChBox.setEnabled(false);
+					im4ChBox.setEnabled(false);
+					im5ChBox.setEnabled(false);
+				}else if(!im2ChBox.isSelected()) {
+					im1ChBox.setEnabled(true);
+					im3ChBox.setEnabled(true);
+					im4ChBox.setEnabled(true);
+					im5ChBox.setEnabled(true);
+				}
+				
+			}
+		});
+		this.im3ChBox.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(im3ChBox.isSelected()) {
+					im1ChBox.setEnabled(false);
+					im2ChBox.setEnabled(false);
+					im4ChBox.setEnabled(false);
+					im5ChBox.setEnabled(false);
+				}else if(!im3ChBox.isSelected()) {
+					im2ChBox.setEnabled(true);
+					im1ChBox.setEnabled(true);
+					im4ChBox.setEnabled(true);
+					im5ChBox.setEnabled(true);
+				}
+				
+			}
+		});
+		this.im4ChBox.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(im4ChBox.isSelected()) {
+					im2ChBox.setEnabled(false);
+					im3ChBox.setEnabled(false);
+					im1ChBox.setEnabled(false);
+					im5ChBox.setEnabled(false);
+				}else if(!im4ChBox.isSelected()) {
+					im2ChBox.setEnabled(true);
+					im3ChBox.setEnabled(true);
+					im1ChBox.setEnabled(true);
+					im5ChBox.setEnabled(true);
+				}
+				
+			}
+		});
+		this.im5ChBox.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(im5ChBox.isSelected()) {
+					im2ChBox.setEnabled(false);
+					im3ChBox.setEnabled(false);
+					im4ChBox.setEnabled(false);
+					im1ChBox.setEnabled(false);
+				}else if(!im5ChBox.isSelected()) {
+					im2ChBox.setEnabled(true);
+					im3ChBox.setEnabled(true);
+					im4ChBox.setEnabled(true);
+					im1ChBox.setEnabled(true);
+				}
+				
+			}
+		});
 	}
 	
-	public static void main(String[] args) {
-		CarListUI carLGUI = new CarListUI();
-		carLGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		carLGUI.setVisible(true);
-		carLGUI.pack();
+	
+	public JCheckBox getIm1ChBox() {
+		return im1ChBox;
 	}
+	public JCheckBox getIm2ChBox() {
+		return im2ChBox;
+	}
+	public JCheckBox getIm3ChBox() {
+		return im3ChBox;
+	}
+	public JCheckBox getIm4ChBox() {
+		return im4ChBox;
+	}
+	public JCheckBox getIm5ChBox() {
+		return im5ChBox;
+	}
+	public JButton getOrderButton() {
+		return orderButton;
+	}
+	public JButton getConfigureButton() {
+		return configureButton;
+	}	
 	
 }
