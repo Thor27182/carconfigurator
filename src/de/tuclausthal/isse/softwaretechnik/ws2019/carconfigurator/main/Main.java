a70bf1aed2548f34fa61d5abb324812978826a1d
YaMeN <YaMeN@DESKTOP-QKBI6AI>
2020-02-05 23:16:33 +0100

    neue Mainklasse erstellt

--- src/de/tuclausthal/isse/softwaretechnik/ws2019/carconfigurator/main/Main.java
+++ src/de/tuclausthal/isse/softwaretechnik/ws2019/carconfigurator/main/Main.java
@@ -1,3 +1,5 @@
+package de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.main;
+
 import de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.controler.impl.ControllerImpl;
 
 public class Main {
