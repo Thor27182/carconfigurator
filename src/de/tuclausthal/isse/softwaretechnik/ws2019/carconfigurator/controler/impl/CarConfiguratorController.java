305d404c8cc0da6425f267988fa0240f40d07db1
ya <ya@YaMeN>
2020-02-05 02:53:14 +0100

    .

---
+++ src/de/tuclausthal/isse/softwaretechnik/ws2019/carconfigurator/controler/impl/CarConfiguratorController.java
@@ -0,0 +1,66 @@
+package de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.controler.impl;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+
+import javax.swing.JComboBox;
+
+import de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.model.impl.Feature;
+import de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.view.impl.CarConfiguratorUI;
+
+public class CarConfiguratorController implements ActionListener, ItemListener {
+
+	private CarConfiguratorUI view;
+	private static Feature feature;
+
+	public CarConfiguratorController(CarConfiguratorUI view) {
+		this.view = view;
+		this.feature = new Feature("1", 1);
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent event) {
+
+		JComboBox s = (JComboBox) event.getSource();
+
+		String item = (String) s.getSelectedItem();
+
+		JComboBox cBamountOfDoors = new JComboBox();
+		JComboBox cBchooseKraftstoff = new JComboBox();
+		JComboBox cBchooseZusatzoptionen = new JComboBox();
+		JComboBox cBchoosePaket = new JComboBox();
+
+		cBamountOfDoors = view.getCBamountOfDoors();
+		cBchooseKraftstoff = view.getCBKraftstoff();
+		cBchooseZusatzoptionen = view.getCBchooseZusatzoptionen();
+		cBchoosePaket = view.getchoosePaket();
+
+		if (item != "- none -") {
+			view.setEnableDropdown(cBamountOfDoors, cBchooseKraftstoff, cBchooseZusatzoptionen, cBchoosePaket);
+		} else if (item == "- none -") {
+			view.setDisableDropdown(cBamountOfDoors, cBchooseKraftstoff, cBchooseZusatzoptionen, cBchoosePaket);
+		}
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent event) {
+		if (event.getStateChange() == ItemEvent.SELECTED) {
+			Object item = event.getItem();
+
+			Object object = event.getSource();
+			if (object == view.getCBchooseZusatzoptionen()) {
+				ArrayList<String> a = new ArrayList<String>();
+				a = feature.getFeatureList();
+				feature.checkAvailableFeatures(a, item);
+			}
+
+			if (item != "- none -") {
+				System.out.print(item);
+			}
+
+		}
+	}
+}
--- src/de/tuclausthal/isse/softwaretechnik/ws2019/carconfigurator/view/impl/CarConfiguratorUI.java
+++ src/de/tuclausthal/isse/softwaretechnik/ws2019/carconfigurator/view/impl/CarConfiguratorUI.java
@@ -1,5 +1,239 @@
 package de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.view.impl;
 
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.controler.impl.CarConfiguratorController;
+import de.tuclausthal.isse.softwaretechnik.ws2019.carconfigurator.model.impl.Feature;
+
 public class CarConfiguratorUI {
+	static Feature feature = new Feature("1", 1);
+	static CarConfiguratorUI view = new CarConfiguratorUI();
+	static ArrayList<String> viewFeatureList = new ArrayList<String>();
+	static ArrayList<String> viewConfigurationpackageList = new ArrayList<String>();
+	static ArrayList<String> viewDoorsList = new ArrayList<String>();
+	static ArrayList<String> viewCarmodelList = new ArrayList<String>();
+	static ArrayList<String> viewFuelList = new ArrayList<String>();
+	static String availableModel[];
+	static String amountOfDoors[];
+	static String kraftstoff[];
+	static String zusatzoptionen[];
+	static String paket[];
+
+	static JComboBox cBamountOfDoors = new JComboBox();
+	static JComboBox cBchooseModel = new JComboBox();
+	static JComboBox cBchooseKraftstoff = new JComboBox();
+	static JComboBox cBchooseZusatzoptionen = new JComboBox();
+	static JComboBox cBchoosePaket = new JComboBox();
+	
+	static JButton  bestellButton;
+	static JButton backButton;
+
+	public CarConfiguratorUI() {
+
+	}
+
+	public static void main(String[] args) {
+		Frame();
+	}
+
+	public static void Frame() {
+
+		JFrame frame = new JFrame("Autokonfigurator");
+		frame.setSize(600, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(6, 2));
+		frame.add(panel);
+
+		// labels
+		JLabel labelChooseModel = new JLabel("wählen Sie ihr Automodell");
+		JLabel labelAmountOfDoors = new JLabel("wählen Sie die Anzahl der Türen");
+		JLabel labelKraftstoff = new JLabel("wählen Sie Kraftstoff");
+		JLabel labelZusatzoptionen = new JLabel("wählen Sie Zusatzoption");
+		JLabel labelPaket = new JLabel("wählen Sie ein Ausstattungspaket");
+
+		 bestellButton = new JButton("bestellen");
+		 backButton = new JButton("zurück");
+
+		// DropdownVariablen
+		setStringArray();
+
+		setCBchooseModel(availableModel);
+		setCBamountOfDoors(amountOfDoors);
+		setCBchooseKraftstoff(kraftstoff);
+		setCBchooseZusatzoptionen(zusatzoptionen);
+		setCBchoosePaket(paket);
+
+		panel.add(labelChooseModel); // Label Model
+		panel.add(cBchooseModel); // Dropdown Model
+		panel.add(labelAmountOfDoors); // label Türen
+		panel.add(cBamountOfDoors); // Dropdown Türen
+		panel.add(labelKraftstoff); // Label Kraftstoff
+		panel.add(cBchooseKraftstoff); // Dropdown Kraftstoff
+		panel.add(labelZusatzoptionen); // Label Zusatzoptionen
+		panel.add(cBchooseZusatzoptionen); // Dropdown Zusatzoptionen
+		panel.add(labelPaket); // Label Kraftstoff
+		panel.add(cBchoosePaket); // Dropdown Kraftstoff
+		panel.add(bestellButton);
+		panel.add(backButton);
+		frame.setVisible(true);
+
+		cBamountOfDoors.setEnabled(false);
+		cBchooseKraftstoff.setEnabled(false);
+		cBchooseZusatzoptionen.setEnabled(false);
+		cBchoosePaket.setEnabled(false);
+
+		cBamountOfDoors.addItemListener(new CarConfiguratorController(view));
+		cBchooseModel.addItemListener(new CarConfiguratorController(view));
+		cBchooseKraftstoff.addItemListener(new CarConfiguratorController(view));
+		cBchooseZusatzoptionen.addItemListener(new CarConfiguratorController(view));
+		cBchoosePaket.addItemListener(new CarConfiguratorController(view));
+
+		// nur auswählbar, wenn ein Fahrzeugtyp gewählt wurde
+		cBchooseModel.addActionListener(new CarConfiguratorController(view));
+	}
+
+	public static void setStringArray() {
+		// DropdownVariablen
+		feature.initializeFeatureList();
+		viewFeatureList = feature.getFeatureList();
+		availableModel = new String[4];
+		availableModel[0] = "- none -";
+		availableModel[1] = "Golf 4";
+		availableModel[2] = "Golf 3";
+		availableModel[3] = "Golf 2";
+		amountOfDoors = new String[3];
+		amountOfDoors[0] = "- none -";
+		amountOfDoors[1] = "3";
+		amountOfDoors[2] = "5";
+		kraftstoff = new String[3];
+		kraftstoff[0] = "- none -";
+		kraftstoff[1] = "Benzin";
+		kraftstoff[2] = "Diesel";
+		// availableModel = viewCarmodelList.toArray(new
+		// String[viewCarmodelList.size()]);
+		// amountOfDoors = viewDoorsList.toArray(new String[viewDoorsList.size()]);
+		// kraftstoff = viewFuelList.toArray(new String[viewFuelList.size()]);
+		zusatzoptionen = viewFeatureList.toArray(new String[viewFeatureList.size()]);
+		// paket = viewConfigurationpackageList.toArray(new
+		// String[viewConfigurationpackageList.size()]);
+		paket = new String[3];
+		paket[0] = "- none -";
+		paket[1] = "Sport-Paket";
+		paket[2] = "Luxus-Paket";
+
+	}
+
+	public void setEnableDropdown(JComboBox cBamountOfDoors, JComboBox cBchooseKraftstoff,
+			JComboBox cBchooseZusatzoptionen, JComboBox cBchoosePaket) {
+		cBamountOfDoors.setEnabled(true);
+		cBchooseKraftstoff.setEnabled(true);
+		cBchooseZusatzoptionen.setEnabled(true);
+		cBchoosePaket.setEnabled(true);
+	}
+
+	public void setDisableDropdown(JComboBox cBamountOfDoors, JComboBox cBchooseKraftstoff,
+			JComboBox cBchooseZusatzoptionen, JComboBox cBchoosePaket) {
+		cBamountOfDoors.setEnabled(false);
+		cBchooseKraftstoff.setEnabled(false);
+		cBchooseZusatzoptionen.setEnabled(false);
+		cBchoosePaket.setEnabled(false);
+		cBamountOfDoors.setSelectedIndex(0);
+		cBchooseKraftstoff.setSelectedIndex(0);
+		cBchooseZusatzoptionen.setSelectedIndex(0);
+		cBchoosePaket.setSelectedIndex(0);
+	}
+
+	public JComboBox getCBKraftstoff() {
+		return cBchooseKraftstoff;
+	}
+
+	public JComboBox getCBamountOfDoors() {
+		return cBamountOfDoors;
+	}
+
+	public JComboBox getCBchooseModel() {
+		return cBchooseModel;
+	}
+
+	public JComboBox getCBchooseZusatzoptionen() {
+		return cBchooseZusatzoptionen;
+	}
+
+	public JComboBox getchoosePaket() {
+		return cBchoosePaket;
+	}
+
+	public static void setCBchooseModel(String[] stringArray) {
+		// model = new DefaultComboBoxModel(stringArray);
+		// cBchooseModel.setModel(model);
+		cBchooseModel = new JComboBox(stringArray);
+	}
+
+	public static void setCBamountOfDoors(String[] stringArray) {
+		// model = new DefaultComboBoxModel(stringArray);
+		// cBamountOfDoors.setModel(model);
+		cBamountOfDoors = new JComboBox(stringArray);
+	}
+
+	public static void setCBchooseKraftstoff(String[] stringArray) {
+		// model = new DefaultComboBoxModel(stringArray);
+		// cBchooseKraftstoff.setModel(model);
+		cBchooseKraftstoff = new JComboBox(stringArray);
+	}
+
+	public static void setCBchooseZusatzoptionen(String[] stringArray) {
+		// model = new DefaultComboBoxModel(stringArray);
+		// cBchooseZusatzoptionen.setModel(model);
+		cBchooseZusatzoptionen = new JComboBox(stringArray);
+	}
+
+	public static void setCBchoosePaket(String[] stringArray) {
+		// model = new DefaultComboBoxModel(stringArray);
+		// cBchoosePaket.setModel(model);
+		cBchoosePaket = new JComboBox(stringArray);
+	}
+
+	public String getCBKraftstoffSelItem() {
+		String item = (String) cBchooseKraftstoff.getSelectedItem();
+		return item;
+	}
+
+	public String getCBModelSelItem() {
+		String item = (String) cBchooseModel.getSelectedItem();
+		return item;
+	}
+
+	public String getCBZusatzoptionenSelItem() {
+		String item = (String) cBchooseZusatzoptionen.getSelectedItem();
+		return item;
+	}
+
+	public String getCBPaketSelItem() {
+		String item = (String) cBchoosePaket.getSelectedItem();
+		return item;
+	}
+
+	public String getCBDoorsSelItem() {
+		String item = (String) cBamountOfDoors.getSelectedItem();
+		return item;
+	}
+
+	public static JButton getBestellButton() {
+		return bestellButton;
+	}
+
+	public static JButton getBackButton() {
+		return backButton;
+	}
+	
 
 }
